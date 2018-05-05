f7a7ead462e427309836a78b6b4b01aa66e692da
Wu Tien Yuan <Michele@WutekiMacBook-Air.local>
2018-05-05 21:13:48 +0800

    update code

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
+	router.Use(cors.Default())
 	InitRouter(router)
 	router.Run(":8080")
 }
